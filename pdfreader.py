7e19e166fd0cc4bdbae463c19b1c9afa176b7fca
Samuel Vidovich <samuel.vidovich@gmail.com>
2021-12-28 17:25:25 -0500

    Use Pillow to read images

--- pdfreader.py
+++ pdfreader.py
@@ -2,8 +2,13 @@ import argparse
 import os
 import sys
 
+from io import BytesIO
+from typing import List
+
+from PIL import Image
+
 from PyPDF2 import PdfFileReader
-from PyPDF2.generic import EncodedStreamObject
+from PyPDF2.generic import DictionaryObject, EncodedStreamObject
 from PyPDF2.pdf import PageObject
 
 DEBUG = False
@@ -23,30 +28,42 @@ def get_pdf_info(reader: PdfFileReader) -> dict:
         'page_layout': reader.getPageLayout(),
     }
 
-def gather_pages(reader: PdfFileReader, page_count: int) -> list:
-    my_page: PageObject = reader.getPage(10)
+def gather_pages(reader: PdfFileReader, page_count: int) -> List[dict]:
     # For now, let's put the pages as images into this list and return it
     output_list = list()
-    page_x_object = my_page['/Resources']['/XObject'].getObject()
-    for entry in page_x_object:
-        if page_x_object[entry]['/Subtype'] == '/Image':
-            encoded_image: EncodedStreamObject = page_x_object[entry]
+    for page_number in range(page_count):
+        page: PageObject = reader.getPage(page_number)
+        page_x_object: DictionaryObject = page['/Resources']['/XObject'].getObject()
 
-            # Workaround for explosive NotImplementedError - grabs
-            # raw image data via private property
-            image_data: bytes = encoded_image._data
-            # TODO: Right now we're doing a lot of hard dictionary accesses
-            # Later this should probably be a little safer
-            width: int = encoded_image['/Width']
-            height: int = encoded_image['/Height']
+        image_number = 0
 
-            image_filter: str = encoded_image['/Filter']
-            image_file_extension: str = filter_to_extension[image_filter]
+        for entry in page_x_object:
+            if page_x_object[entry]['/Subtype'] == '/Image':
+                encoded_image: EncodedStreamObject = page_x_object[entry]
 
-            image_color_space = encoded_image['/ColorSpace']
-            image_mode = 'RGB' if image_color_space == '/DeviceRGB' else 'P'
-            print(f"Found an image whose mode is {image_mode}, of size {width} x {height}")
+                # Workaround for explosive NotImplementedError - grabs
+                # raw image data via private property
+                image_data: bytes = encoded_image._data
+                # TODO: Right now we're doing a lot of hard dictionary accesses
+                # Later this should probably be a little safer
+                width: int = encoded_image['/Width']
+                height: int = encoded_image['/Height']
 
+                image_filter: str = encoded_image['/Filter']
+                image_file_extension: str = filter_to_extension[image_filter]
+
+                image_color_space = encoded_image['/ColorSpace']
+                image_mode = 'RGB' if image_color_space == '/DeviceRGB' else 'P'
+                print(f'Attempting to load image {image_number} on page {page_number}.')
+                image = {
+                    'name': f'{page_number}_{image_number}.{image_file_extension}',
+                    'image_data': Image.frombytes(image_mode, (width, height), image_data) \
+                        if image_mode == 'RGB' \
+                            else Image.open(BytesIO(image_data))
+                }
+                output_list.append(image)
+                image_number += 1
+    return output_list
 
 def main():
     parser = argparse.ArgumentParser()
@@ -58,7 +75,9 @@ def main():
     reader = PdfFileReader(input_file, strict=DEBUG, warndest=sys.stderr if DEBUG else os.devnull)
     info = get_pdf_info(reader)
     page_count: int = info['page_count']
-    gather_pages(reader, page_count)
+    document_images: List[dict] = gather_pages(reader, page_count)
+    print('Got some images.')
+    print(document_images)
 
 
 if __name__ == '__main__':
