f60173657e506cd9a48bc6cfd76b09f0fa20ad81
Samuel Vidovich <samuel.vidovich@gmail.com>
2021-12-28 17:08:55 -0500

    Add additional data. TODO use PIL

--- pdfreader.py
+++ pdfreader.py
@@ -6,7 +6,13 @@ from PyPDF2 import PdfFileReader
 from PyPDF2.generic import EncodedStreamObject
 from PyPDF2.pdf import PageObject
 
-DEBUG=False
+DEBUG = False
+
+filter_to_extension = {
+    '/FlateDecode': '.png',
+    '/DCTDecode': '.jpg',
+    '/JPXDecode': '.jp2',
+}
 
 def get_pdf_info(reader: PdfFileReader) -> dict:
     standard_info = reader.documentInfo
@@ -19,26 +25,28 @@ def get_pdf_info(reader: PdfFileReader) -> dict:
 
 def gather_pages(reader: PdfFileReader, page_count: int) -> list:
     my_page: PageObject = reader.getPage(10)
-    from pprint import pprint
+    # For now, let's put the pages as images into this list and return it
+    output_list = list()
     page_x_object = my_page['/Resources']['/XObject'].getObject()
     for entry in page_x_object:
         if page_x_object[entry]['/Subtype'] == '/Image':
             encoded_image: EncodedStreamObject = page_x_object[entry]
-            print(type(encoded_image))
-            print(encoded_image.items())
-            width: int = encoded_image['/Width']
-            height: int = encoded_image['/Height']
+
             # Workaround for explosive NotImplementedError - grabs
             # raw image data via private property
-            image_data = encoded_image._data
+            image_data: bytes = encoded_image._data
+            # TODO: Right now we're doing a lot of hard dictionary accesses
+            # Later this should probably be a little safer
+            width: int = encoded_image['/Width']
+            height: int = encoded_image['/Height']
+
+            image_filter: str = encoded_image['/Filter']
+            image_file_extension: str = filter_to_extension[image_filter]
+
             image_color_space = encoded_image['/ColorSpace']
             image_mode = 'RGB' if image_color_space == '/DeviceRGB' else 'P'
             print(f"Found an image whose mode is {image_mode}, of size {width} x {height}")
-    
-    page_contents = my_page.getContents()
-    # print(dir(page_contents))
-    # print(page_contents)
-    # print(page_contents.getXmpMetadata())
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -49,7 +57,6 @@ def main():
 
     reader = PdfFileReader(input_file, strict=DEBUG, warndest=sys.stderr if DEBUG else os.devnull)
     info = get_pdf_info(reader)
-    print(info)
     page_count: int = info['page_count']
     gather_pages(reader, page_count)
 
